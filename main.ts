add8e516bf39354fe1277a0f57a64ed6a6e427e6
billal_boudjoghra <bboudjoghra@deezer.com>
2024-02-11 22:47:26 +0100

    Working breadcrumb !

--- main.ts
+++ main.ts
@@ -1,134 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {getAPI, DataviewApi} from "obsidian-dataview";
+import { openOrSwitch } from 'obsidian-community-lib/dist/utils';
+import {Arr} from "tern";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class BreadcrumbPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
+		const statusBarItem = this.addStatusBarItem();
+		// FIXME? this seems tricky
+		//this.registerEvent(this.app.metadataCache.on("dataview:index-ready", () => {
+				this.registerEvent(this.app.workspace.on('file-open', async () => {
+					statusBarItem.setText("")
+					const file = this.app.workspace.getActiveFile()
+					const dv = getAPI(this.app);
+					if (dv && file) {
+						const pages: Set<string> = new Set();
+						const stack = [file.path];
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+						while (stack.length > 0) {
+							const elem = stack.pop();
+							const meta = dv.page(elem);
+							if (!meta) continue;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+							for (const inlink of meta.file.inlinks.array()) {
+								if (!inlink.path.contains("*") && !inlink.path.contains("!")) continue
+								if (pages.has(inlink.path)) continue;
+								pages.add(inlink.path);
+								stack.push(inlink.path);
+							}
+						}
+						Array.from(pages).reverse().forEach(p => {
+							const page = dv.page(p).file
+							const link = statusBarItem.createEl("a", {
+								cls: "internal-link 1",
+								href: page.path,
+								text: page.name,
+							})
+							link.onClickEvent(async (evt: MouseEvent) => {
+								await openOrSwitch(page.path, evt)
+							})
+							statusBarItem.createEl("p", {
+								text: " > "
+							})
+						})
+					} else {
+						console.log("failed to load")
 					}
+			//	}))
+			})
+		)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
